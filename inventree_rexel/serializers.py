39915c40e2d5fc9282a5834096cc38a96fd6cb03
Philipboy881 <114617718+Philipboy881@users.noreply.github.com>
2024-12-31 14:40:30 +0100

    Update serializers.py

--- inventree_rexel/serializers.py
+++ inventree_rexel/serializers.py
@@ -1,15 +1,4 @@
-from rest_framework import serializers
-
-
 class RexelRequestSerializer(serializers.Serializer):
     """Serializer voor validatie van Rexel data."""
-    product_number = serializers.CharField(
-        max_length=255,
-        required=True,
-        source='product_number'
-    )
-    part_number = serializers.CharField(
-        max_length=255,
-        required=True,
-        source='part_number'
-    )
+    product_number = serializers.CharField(max_length=255, required=True)
+    part_number = serializers.CharField(max_length=255, required=True)
