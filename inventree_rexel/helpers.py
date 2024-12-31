c67837134f8d99e5096830c7261716a8bcbf75f5
Philipboy881 <114617718+Philipboy881@users.noreply.github.com>
2024-12-31 14:02:44 +0100

    Update helpers.py

--- inventree_rexel/helpers.py
+++ inventree_rexel/helpers.py
@@ -2,13 +2,13 @@ def process_rexel_data(data):
     """
     Verwerk Rexel data en geef een resultaat terug.
     """
-    product_number = data['productNumber']
-    part_number = data['partNumber']
+    product_number = data['product_number']
+    part_number = data['part_number']
 
     # Verwerking (voorbeeld)
     return {
-        'productNumber': product_number,
-        'partNumber': part_number,
+        'product_number': product_number,
+        'part_number': part_number,
         'status': 'success',
         'message': f'Processed part {part_number} for product {product_number}.'
     }
